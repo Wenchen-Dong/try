acc79737a08d7678852ee848ba168e801cf767cd
WenchenDong <1432007@tongji.edu.cn>
2016-11-09 17:11:30 +0800

    change style

--- source/conf.py
+++ source/conf.py
@@ -146,7 +146,7 @@ todo_include_todos = False
 #
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
-    html_theme = 'default'
+    html_theme = 'nature'
 else:
   html_theme = 'alabaster'
 
